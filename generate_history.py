d124966a8620220408fdc85c23f60e0bc1fd7e59
César Caramazana Zarzosa <88673785+CesarCaramazana@users.noreply.github.com>
2022-11-30 10:50:22 +0100

    Add files via upload

--- generate_history.py
+++ generate_history.py
@@ -2,6 +2,7 @@ import numpy as np
 import wavedrom
 import math
 import os
+import shutil
 
 resolution = 30 #Subsampling factor
 
@@ -38,7 +39,7 @@ def generate_action_duration(history):
 
 
 
-def generate_signal(history):
+def generate_signal(history, rwd_format=False):
 	"""
 	Generates a string associated to the history of actions, in the format required to generate a plot with wavedrom.
 	Particular actions are assigned a color code for the display of the bars.
@@ -62,26 +63,34 @@ def generate_signal(history):
 	for i, action in enumerate(history):
 		duration = math.floor(action[1]/resolution)		
 		
-		if action[0] == 'do nothing':
-			color_code = "x"
+		if rwd_format:
+			if action[0] == 0:
+				color_code = "z"
+			else:
+				color_code = "2"
+				actions.append(math.floor(action[0]))	
 		
-		elif action[0] == 'other manipulation':
-			color_code = "5"
-			actions.append('other')
+		else:
+			if action[0] == 'do nothing':
+				color_code = "x"
+			
+			elif action[0] == 'other manipulation':
+				color_code = "5"
+				actions.append('other')
 
-		elif action[0] == 'Predicting...':
-			color_code = "6"
-			#actions.append(action[0])
-			actions.append("...")
-		
-		elif action[0] == 'Waiting for evaluation...' or action[0] == 'Waiting for robot action...':
-			color_code = "9"
-			#actions.append(action[0])
-			actions.append('wait')
+			elif action[0] == 'Predicting...':
+				color_code = "6"
+				#actions.append(action[0])
+				actions.append("...")
+			
+			elif action[0] == 'Waiting for evaluation...' or action[0] == 'Waiting for robot action...':
+				color_code = "9"
+				#actions.append(action[0])
+				actions.append('wait')
 
-		else: 
-			color_code = "3"
-			actions.append(action[0])	
+			else: 
+				color_code = "3"
+				actions.append(action[0])	
 		
 		waveform = waveform + color_code + "."*(duration-1)
 		
@@ -96,6 +105,7 @@ def create_graph(save_path, file_id):
 	Reads a file name/file id, in the format .npz, containing the history of an interaction between robot and human, and generates a plot representing the states of both agents during the course of the interaction. 
 	
 	Input:
+		save_path: (str) path where the graphs are saved at.
 		file_id: (int) .npz file identifier. 
 		
 	Output:
@@ -115,17 +125,27 @@ def create_graph(save_path, file_id):
 	readpath = path + str(file_id) + ".npz" 
 	history = np.load(readpath)
 	
+	copy_path = full_path + str(file_id) + ".npz"
+	shutil.move(readpath, copy_path)
+	
 	human = history['h_history'] 
 	robot = history['r_history']
+	reward = history['rwd_history']
 	
 	r_history=[robot[i].item() for i in range(0,len(robot),resolution)] #Subsampling: taking the first of every 'resolution' elements
 	h_history=[human[i].item() for i in range(0,len(human),resolution)]
+	rwd_history = [min(reward[i:i+resolution])[0] for i in range(0, len(reward), resolution)]
+
 	
 	human_history = generate_action_duration(h_history)
 	robot_history = generate_action_duration(r_history)
+	reward_history = generate_action_duration(rwd_history)
+	
+	print(reward_history)
 	
 	h_actions, h_waveform = generate_signal(human_history)
 	r_actions, r_waveform = generate_signal(robot_history)
+	rwd_values, rwd_waveform = generate_signal(reward_history, rwd_format=True)
 	
 	
 	clk = "P" + "."*(len(r_waveform)-1) #Clock signal
@@ -135,7 +155,9 @@ def create_graph(save_path, file_id):
 	d = { "signal": [
 		{ "name": clock, "wave": clk},
 		{ "name": "Human", "wave": h_waveform, "data": h_actions},
-		{ "name": "Robot",  "wave": r_waveform ,"data": r_actions }]}
+		{ "name": "Robot",  "wave": r_waveform ,"data": r_actions },
+		{ "name": "Reward", "wave": rwd_waveform, "data": rwd_values}
+	]}
 	
 	
 	svg = wavedrom.render(str(d))	
